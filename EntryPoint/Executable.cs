3d3cf649451971cf14fcf032899b6eee3429e652
ManIkWeet <not@4u.com>
2017-11-03 16:47:38 +0100

    Full project

---
+++ EntryPoint/Executable.cs
@@ -0,0 +1,17 @@
+﻿using MainProject;
+using System;
+using System.Windows;
+
+namespace EntryPoint
+{
+    internal class Executable
+    {
+        [STAThread]
+        private static void Main()
+        {
+
+            var app = new Application();
+            app.Run(new MainWindow());
+        }
+    }
+}
---
+++ MainProject/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MainProject
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
